c5ab1a330add768cd820763bb914142bf48ffdb5
Shevchenko-Py-lab <shevchenko.logist@mail.ru>
2022-02-26 01:31:33 +0300

    Check_first_commit

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from uuid import uuid4
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# class User(AbstractUser):
+#     uid = models.UUIDField(primary_key=True, default=uuid4)
+
+class User(models.Model):
+    uid = models.UUIDField(primary_key=True,
+                           default=uuid4)
+    user_name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64,
+                             unique=True)
+
